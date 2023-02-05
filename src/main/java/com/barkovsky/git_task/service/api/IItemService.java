f9e0d4a9cd7da1d5ce3f7625533a51ec1c8fadd3
SleetFire <barkovskiy2000@gmail.com>
2023-02-05 23:24:10 +0300

    docs: add docs for item's api

--- src/main/java/com/barkovsky/git_task/service/api/IItemService.java
+++ src/main/java/com/barkovsky/git_task/service/api/IItemService.java
@@ -7,10 +7,24 @@ import java.util.UUID;
 
 public interface IItemService {
 
+    /**
+     * Creating new item in database
+     * @param item our item
+     * @return item with uuid
+     */
     Item create(Item item);
 
+    /**
+     * Getting item from database by uuid
+     * @param uuid item's uuid
+     * @return item from database
+     */
     Item get(UUID uuid);
 
+    /**
+     * Getting list of items
+     * @return list of items
+     */
     List<Item> getAll();
 
 }
