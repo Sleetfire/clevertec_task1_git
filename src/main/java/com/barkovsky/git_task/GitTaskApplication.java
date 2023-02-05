df963fe029bdd575ecdf2e1e69860a0f8316b841
SleetFire <barkovskiy2000@gmail.com>
2023-02-05 20:51:57 +0300

    add project's init files

---
+++ src/main/java/com/barkovsky/git_task/GitTaskApplication.java
@@ -0,0 +1,15 @@
+package com.barkovsky.git_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.barkovsky.git_task")
+public class GitTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTaskApplication.class, args);
+	}
+
+}
