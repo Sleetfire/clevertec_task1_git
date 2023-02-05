2518c5f67622c671e596cff18204cc58ef970cc1
SleetFire <barkovskiy2000@gmail.com>
2023-02-05 23:16:06 +0300

    fix: fix base packages path for repository

--- src/main/java/com/barkovsky/git_task/GitTaskApplication.java
+++ src/main/java/com/barkovsky/git_task/GitTaskApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.barkovsky.git_task")
+@EnableJpaRepositories(basePackages = "com.barkovsky.git_task.repository.api")
 public class GitTaskApplication {
 
 	public static void main(String[] args) {
